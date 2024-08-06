91556087b2836ab9a604fe20c7db29060914880a
Federico Pigato <federico.pigato2001@gmail.com>
2024-08-06 21:12:03 +0200

    adjusted clickable area to link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,11 +3,11 @@ import './Home.css';
 const Home = () => {
     return (
         <div className="Home">
-            <a href="/cheapFlights">
-                <dotlottie-player src="https://lottie.host/b46fd541-2c1b-49e0-91fc-88d1067765c6/4Lcjwv8xQr.json" background="transparent" speed="0.8" loop autoplay></dotlottie-player>
+            <a href="/cheapFlights" className="iconToPage">
+                <dotlottie-player style={{ pointerEvents: "none"}}src="https://lottie.host/b46fd541-2c1b-49e0-91fc-88d1067765c6/4Lcjwv8xQr.json" background="transparent" speed="0.8" loop autoplay></dotlottie-player>
             </a>
-            <a href="/bestDepartFlights" >
-                <dotlottie-player style={{ transform: 'scale(2)' }} src="https://lottie.host/802f258b-8899-4e64-8c35-5f9dca55d76f/LXY2aTeTTs.json" background="transparent" speed="1" loop autoplay></dotlottie-player>
+            <a href="/bestDepartFlights" className="iconToPage">
+                <dotlottie-player style={{ transform: 'scale(2)', pointerEvents: "none", overflow: "hidden"}} src="https://lottie.host/802f258b-8899-4e64-8c35-5f9dca55d76f/LXY2aTeTTs.json" background="transparent" speed="1" loop autoplay></dotlottie-player>
             </a>
         </div>
     );
